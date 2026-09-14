7ae504bf693778656ecc51975284a81dd6d1f847
rull0915 <ryu.915.bangdreamer@gmail.com>
2026-09-15 02:01:09 +0900

    完成品SOにlayerを追加

--- Assets/Scripts/LaunchScene/FireworksParticleController.cs
+++ Assets/Scripts/LaunchScene/FireworksParticleController.cs
@@ -2,7 +2,11 @@ using UnityEngine;
 
 public class FireworksParticleController : MonoBehaviour
 {
+    // Š®¬•i‚ÌScriptableObject
+    [SerializeField] private CompletedFireworks m_fireworks;
 
+    // ‘€ì‘ÎÛ‚ÌParticleSystem
+    [SerializeField] private ParticleSystem[] m_particleSystems = new ParticleSystem[3];
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
