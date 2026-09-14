addade479b136857d9e604c4076dc11cc13f8e8a
rull0915 <ryu.915.bangdreamer@gmail.com>
2026-09-14 18:18:46 +0900

    星選択の作成

--- Assets/Scripts/SelectScene/SelectSceneManager.cs
+++ Assets/Scripts/SelectScene/SelectSceneManager.cs
@@ -4,14 +4,22 @@ using UnityEngine.UI;
 
 public class SelectSceneManager : MonoBehaviour
 {
+    // SO
     [SerializeField] private OuterShell m_selectShell;
+    [SerializeField] private SelectedMaterials m_selectMaterials;
 
     // ボタンの一覧
     ShellButton m_selectShellButton;
     [SerializeField] private List<ShellButton> m_shellButtons = new List<ShellButton>();
 
+    // 星ボタンの一覧
+    [SerializeField] private List<StarButton> m_starButtons = new List<StarButton>();
+
     // 選択カーソル
     [SerializeField] private RectTransform m_selectCursor;
+
+    // 星選択管理
+    [SerializeField] private StarButtonManager m_starButtonManager;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -46,12 +54,24 @@ public class SelectSceneManager : MonoBehaviour
         
     }
 
-    // 決定関数
-    public void Decide()
+    // 外殻決定関数
+    public void ShellDecide()
     {
         if (!m_selectShell || !m_selectShellButton) return;
 
         m_selectShell.size = m_selectShellButton.GetSize();
         m_selectShell.material = m_selectShellButton.GetMaterial();
+    }
+
+    // 全体決定関数
+    public void Decide()
+    {
+        // 初期化
+        m_selectMaterials.stars.Clear();
+
+        foreach (var star in m_starButtonManager.GetManagedButtons())
+        {
+            m_selectMaterials.stars.Add(new FireworkStar());
+        }
     }
 }
--- Assets/Scripts/Sounds/SoundManager.cs
+++ Assets/Scripts/Sounds/SoundManager.cs
@@ -13,7 +13,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 //====================================================//
 // ƒNƒ‰ƒXéŒ¾
